1f31bf3ec9cf60f60544869f8435dbd6f606695d
jdenquin <jdenquin@gmail.com>
2025-07-21 17:25:55 +0200

    remove useless comments

--- mcp/src/tools/invoice.rs
+++ mcp/src/tools/invoice.rs
@@ -12,7 +12,6 @@ use lago_types::{
     filters::invoice::InvoiceFilters,
     models::{
         PaginationParams,
-        //Invoice,
         InvoiceType,
         InvoiceStatus,
         InvoicePaymentStatus,
